6a868d7d8816dfd0167f08e1f1d0f6c8b7626769
Adrian Lara <93059829+adrian1lara@users.noreply.github.com>
2024-10-29 14:47:12 -0500

    add strip inputs, path joins and vefication

--- main.py
+++ main.py
@@ -11,34 +11,37 @@ strip_src_path = src_path.strip()
 strip_dest_path = dest_path.strip()
 
 # input of the exclude path
-exclude_file = input('name of the exclude file')
+exclude_file = input('name of the exclude file: ').strip()
 # validate if the two directory path exists
-if os.path.exists(strip_src_path and strip_dest_path):
+if os.path.exists(strip_src_path) and os.path.exists(strip_dest_path):
     # save the listed files in a variable
     dir_list = os.listdir(strip_src_path)
     print("Files and directories in '", strip_src_path, "' :")
     print(dir_list)
 
     for file_name in dir_list:
-        source = strip_dest_path + file_name
+        if file_name  == exclude_file:
+            continue
 
-        destination = strip_dest_path + file_name
+        source = os.path.join(strip_src_path, file_name)
+
+        destination = os.path.join(strip_dest_path, file_name)
 
         shutil.copy(source, destination)
         print('copied', file_name)
 
-    path_save_zip = input('Where save the zip: ')
+    path_save_zip = input('Where to save the zip: ').strip()
 
-    strip_path_save_zip = path_save_zip.strip()
+
     # Creating the ZIP file
-    archived = shutil.make_archive(strip_dest_path, 'zip', strip_path_save_zip)
+    archived = shutil.make_archive(path_save_zip, 'zip', strip_dest_path)
 
-    if os.path.exists(strip_path_save_zip):
+    if os.path.exists(path_save_zip + '.zip'):
         print(archived)
     else: 
         print("ZIP file not created")
 else:
-    print("Directory path not exists")
+    print("One or both Directory paths not exists")
 
 
 
